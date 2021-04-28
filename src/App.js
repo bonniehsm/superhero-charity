7bffc21e93f60b8142e0e53071579693bc4db8ab
Bonnie <bonnie.hung@hotmail.ca>
2021-04-28 13:06:10 -0400

    added Home and Donation containers, routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.scss';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import HomeContainer from './containers/HomeContainer'; 
+import DonationContainer from './containers/DonationContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={HomeContainer}/>
+          <Route path="/donation" exact component={DonationContainer}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/DonationContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class DonationContainer extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                <header>
+                    <h1>DonationContainer</h1>
+                </header>              
+            </div>
+        )
+    }
+}
+
+export default DonationContainer;
